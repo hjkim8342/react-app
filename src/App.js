9b08828c8ee80948689809faa64bd61279d99746
hjkim8342 <hjkim8342@gmail.com>
2020-07-05 02:15:35 +0900

    최초등록

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Toc from './components/Toc';
+import Content from './components/Content';
+import Subject from './components/Subject';
+import './App.css';
+
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="World Wide Web!"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <Toc></Toc>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
+}
+export default App;
