889dbf4ed47ec243061455eba4863638fd425edb
Ki-Ril <fenixrane@gmail.com>
2020-07-10 00:02:01 +0900

    16강까지 커밋

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 class Subject extends Component {
-    render () {
+    render() {
         return (
             <header>
-                <h1>{this.props.title}</h1>
+                <h1><a href='/'>{this.props.title}</a></h1>
                 {this.props.sub}
             </header>
         );
